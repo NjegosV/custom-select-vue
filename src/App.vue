73b9d020eff439ed75151a7d255cf289a1328942
NjegosV <njevujov@osb-ag.de>
2022-03-10 08:49:11 +0100

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import InputSelect from "./components/Inputs/InputSelect.vue";
+</script>
+
+<template>
+  <div class="center">
+    <InputSelect />
+  </div>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-block: 4rem;
+}
+</style>
